910e7d33acc964f71838721c10663df76ed35be6
narasimhanaidu <talarinarasimhanaidu1@gmail.com>
2023-11-27 20:44:26 +0530

    grou[ping score

---
+++ grouping_score.py
@@ -0,0 +1,18 @@
+def get_color_score_pairs_dict(ball_score_list):
+    color_score_pairs_dict = {}
+    for item in ball_score_list:
+        pair = item.split(":")
+        key, value = pair[0], int(pair[1])
+        if key in color_score_pairs_dict:
+            score = color_score_pairs_dict[key]
+            color_score_pairs_dict[key] = score + value
+
+        else:
+            color_score_pairs_dict[key] = value
+
+    return color_score_pairs_dict
+ball_score_list = input().split(",")
+color_score_pairs_dict = get_color_score_pairs_dict(ball_score_list)
+color_score_pairs_dict_items = color_score_pairs_dict.items()
+color_score_pairs_dict_items = sorted(color_score_pairs_dict_items)
+print(color_score_pairs_dict_items)
