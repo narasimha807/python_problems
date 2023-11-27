50d0b7ed0324b4a82ebd59024ee82ffd95fafd8e
narasimhanaidu <talarinarasimhanaidu1@gmail.com>
2023-11-27 20:46:08 +0530

    grouping_score

--- grouping_score.py
+++ grouping_score.py
@@ -1,3 +1,4 @@
+#grouping_Score
 def get_color_score_pairs_dict(ball_score_list):
     color_score_pairs_dict = {}
     for item in ball_score_list:
@@ -16,3 +17,4 @@ color_score_pairs_dict = get_color_score_pairs_dict(ball_score_list)
 color_score_pairs_dict_items = color_score_pairs_dict.items()
 color_score_pairs_dict_items = sorted(color_score_pairs_dict_items)
 print(color_score_pairs_dict_items)
+
